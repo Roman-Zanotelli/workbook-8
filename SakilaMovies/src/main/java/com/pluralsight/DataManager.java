58915ec45bf0a7d24a4f7b9df272e9f7f0065555
Roman-Zanotelli <Roman-Zanotelli@github.com>
2025-06-09 15:32:38 -0400

    Updated Prepared Statement

--- SakilaMovies/src/main/java/com/pluralsight/DataManager.java
+++ SakilaMovies/src/main/java/com/pluralsight/DataManager.java
@@ -4,6 +4,7 @@ package com.pluralsight;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -39,13 +40,19 @@ public class DataManager {
     public static List<Actor> getActors(String firstName, String lastName){
         boolean firstExists = !firstName.isBlank();
         boolean lastExists = !lastName.isBlank();
-        String query = String.format(
-                "SELECT * FROM actor%s%s",
-                firstExists ? String.format(" WHERE first_name = '%s'", firstName) : "",
-                lastExists ? String.format(" %s last_name = '%s'", firstExists ? "AND" : "WHERE", lastName) : ""
-        );
-        System.out.println(query);
-        try(ResultSet res = sqlConnection.prepareStatement(query).executeQuery()){
+        PreparedStatement query;
+        try {
+            query = sqlConnection.prepareStatement(String.format(
+                    "SELECT * FROM actor%s%s",
+                    firstExists ? " WHERE first_name = ?" : "",
+                    lastExists ? String.format(" %s last_name = ?", firstExists ? "AND" : "WHERE") : ""
+            ));
+            if(firstExists || lastExists) query.setString(1, firstExists ? firstName : lastName);
+            if(firstExists && lastExists) query.setString(2, lastName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try(ResultSet res = query.executeQuery()){
             List<Actor> actors = new ArrayList<>();
             if(!res.next()) return null; else do{
                 actors.add(new Actor(res.getString("first_name"), res.getString("last_name"), res.getInt("actor_id")));
@@ -59,7 +66,14 @@ public class DataManager {
         return null;
     }
     public static List<Film> getFilmsByActorID(int actorID){
-        try(ResultSet res = sqlConnection.prepareStatement(String.format("SELECT * FROM film WHERE film_id IN(SELECT film_id FROM film_actor WHERE actor_id = '%d')", actorID)).executeQuery()){
+        PreparedStatement query;
+        try {
+            query = sqlConnection.prepareStatement("SELECT * FROM film WHERE film_id IN(SELECT film_id FROM film_actor WHERE actor_id = ?)");
+            query.setInt(1, actorID);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try(ResultSet res = query.executeQuery()){
             List<Film> films = new ArrayList<>();
             if(!res.next()) return null; else do{
                 films.add(new Film(res.getInt("film_id"), res.getInt("release_year"), res.getInt("length"), res.getString("title"), res.getString("description")));
